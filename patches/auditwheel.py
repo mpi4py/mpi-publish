ac77bfac1a5e7238ff76ad6a67c9b0ae0e6501c8
Lisandro Dalcin <dalcinl@gmail.com>
2025-04-09 20:45:09 +0300

    Fix wheel file order in dist-info directories on Linux

--- patches/auditwheel.py
+++ patches/auditwheel.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python
+import pathlib
 import sys
 
+import auditwheel.tools
 from auditwheel.main import main
+
+aw_walk = auditwheel.tools.walk
+
+def walk(topdir):
+    topdir = pathlib.Path(topdir).resolve(strict=True)
+    for dirpath, dirnames, filenames in aw_walk(topdir):
+        if (
+            dirpath.suffix == ".dist-info"
+            and dirpath.parent == topdir
+        ):
+            # list any dist-info/licenses/LICENSE* files
+            for dirname in dirnames:
+                for dn, _, fns in walk(dirpath / dirname):
+                    for fn in fns:
+                        fn = (dn / fn).relative_to(dirpath)
+                        filenames.append(str(fn))
+            del dirnames[:]
+            # list any dist-info/RECORD file last
+            if "RECORD" in filenames:
+                filenames.remove("RECORD")
+                filenames.append("RECORD")
+        yield dirpath, dirnames, filenames
+
+auditwheel.tools.walk = walk
 
 if "repair" in sys.argv:
     sys.argv.append("--only-plat")
