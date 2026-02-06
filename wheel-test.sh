7f9795ec919377169fd4f4905186e78ac9489cd4
Lisandro Dalcin <dalcinl@gmail.com>
2026-02-06 21:51:44 +0300

    Test with mpi4py

--- wheel-test.sh
+++ wheel-test.sh
@@ -22,4 +22,11 @@ RUN source "$venv"/bin/activate
 RUN python -m pip install "$mpispec" \
     --no-index --find-links "${1:-wheelhouse}"
 
-RUN ./cibw-check-mpi.sh
+RUN source ./cibw-check-mpi.sh
+
+RUN python -m pip install mpi4py
+RUN mpiexec -n 1 python -m mpi4py --mpi-library
+RUN mpiexec -n 1 python -m mpi4py --mpi-std-version
+RUN mpiexec -n 1 python -m mpi4py --mpi-lib-version
+RUN mpiexec -n 3 python -m mpi4py.bench helloworld
+RUN mpiexec -n 3 python -m mpi4py.bench ringtest
